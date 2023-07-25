60d5aaf4369a9aa17baa4b5b03eee6d6ee59ac3a
Buzgau Catalin <buzgaucatalin@yahoo.com>
2023-07-25 17:23:35 +0300

    Allows multiple linear regression with/without intercept

--- regression.py
+++ regression.py
@@ -8,12 +8,7 @@ class CustomLinearRegression:
         self.coefficient = None
         self.intercept = None
 
-    def fit(self, X: list[float], y: list[float]):
-        if not isinstance(X, np.ndarray):
-            X = np.array(X)
-        if not isinstance(y, np.ndarray):
-            y = np.array(y)
-
+    def fit(self, X: np.ndarray, y: np.ndarray):
         if self.fit_intercept:
             X = np.column_stack((np.ones(len(X)), X))
 
@@ -24,12 +19,21 @@ class CustomLinearRegression:
             self.intercept = self.coefficient[0]
             self.coefficient = self.coefficient[1:]
 
+    def predict(self, X):
+        if self.intercept is not None:
+            self.coefficient.insert(self.intercept, 0)
+
+        return X.dot(self.coefficient)
+
 
 if __name__ == '__main__':
-    x = [4.0, 4.5, 5, 5.5, 6.0, 6.5, 7.0]
+    x = [4, 4.5, 5, 5.5, 6, 6.5, 7]
+    w = [1, -3, 2, 5, 0, 3, 6]
+    z = [11, 15, 12, 9, 18, 13, 16]
     y = [33, 42, 45, 51, 53, 61, 62]
-    regression = CustomLinearRegression()
-    regression.fit(x, y)
 
-    output_dict = {'Intercept': regression.intercept, 'Coefficient': regression.coefficient}
-    print(output_dict)
+    regression = CustomLinearRegression(fit_intercept=False)
+    X = np.column_stack((np.array(x), np.array(w), np.array(z)))
+    regression.fit(X, np.array(y))
+    y_pred = regression.predict(X)
+    print(y_pred)
