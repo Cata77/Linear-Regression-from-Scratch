ffbc79ee63ee016a0af75df6407b9997de004dda
Buzgau Catalin <buzgaucatalin@yahoo.com>
2023-07-27 21:10:57 +0300

    Implement R2 and RMSE

--- regression.py
+++ regression.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 
@@ -20,20 +22,33 @@ class CustomLinearRegression:
             self.coefficient = self.coefficient[1:]
 
     def predict(self, X):
+        if self.fit_intercept:
+            X = np.column_stack((np.ones(len(X)), X))
+
         if self.intercept is not None:
-            self.coefficient.insert(self.intercept, 0)
+            self.coefficient = np.insert(self.coefficient, 0, self.intercept)
 
         return X.dot(self.coefficient)
 
+    def r2_score(self, y, y_hat):
+        y_mean = np.mean(y)
+        return 1 - np.sum((y - y_hat) ** 2) / np.sum((y - y_mean) ** 2)
+
+    def rmse(self, y, y_hat):
+        MSE = 1 / len(y) * np.sum((y - y_hat) ** 2)
+        return math.sqrt(MSE)
+
 
 if __name__ == '__main__':
-    x = [4, 4.5, 5, 5.5, 6, 6.5, 7]
-    w = [1, -3, 2, 5, 0, 3, 6]
-    z = [11, 15, 12, 9, 18, 13, 16]
-    y = [33, 42, 45, 51, 53, 61, 62]
-
-    regression = CustomLinearRegression(fit_intercept=False)
-    X = np.column_stack((np.array(x), np.array(w), np.array(z)))
-    regression.fit(X, np.array(y))
+    capacity = [0.9, 0.5, 1.75, 2.0, 1.4, 1.5, 3.0, 1.1, 2.6, 1.9]
+    age = [11, 11, 9, 8, 7, 7, 6, 5, 5, 4]
+    cost_per_ton = [21.95, 27.18, 16.9, 15.37, 16.03, 18.15, 14.22, 18.72, 15.4, 14.69]
+    regression = CustomLinearRegression(fit_intercept=True)
+    X = np.column_stack((np.array(capacity), np.array(age)))
+    regression.fit(X, np.array(cost_per_ton))
     y_pred = regression.predict(X)
-    print(y_pred)
+
+    output_dict = {'Intercept': regression.intercept, 'Coefficient': np.array(regression.coefficient[1:]),
+                   'R2': regression.r2_score(np.array(cost_per_ton), y_pred),
+                   'RMSE': regression.rmse(np.array(cost_per_ton), y_pred)}
+    print(output_dict)
